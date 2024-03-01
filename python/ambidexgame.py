725649d92fcca6fe1450875cb1bfdbe6aa4b4def
missasumay <missasumay@outlook.com>
2024-03-01 23:20:56 +0100

    Two new projects
    Two fairly simple projects - Ambidex Game (based off of Virtue's Last Reward) and Pokedex app.

---
+++ python/ambidexgame.py
@@ -0,0 +1,144 @@
+import random
+import sys
+
+# required data
+
+ambidex = ["ALLY", "BETRAY"]
+enemy_names = ["Zero", "Sigma", "Phi", "Alice", "Luna", "Dio", "Quark", "Tenmyouji", "Clover", "K"]
+true_false = ["true", "false"]
+
+print("The Ambidex Game will now begin.\n")
+print("Rules are as follows:\n1. If both you and your opponent choose ALLY, both of you will receive 2 points.\n2. If both of you will choose BETRAY, no points will be added.\n3. If you choose ALLY and your opponent chooses BETRAY, you lose 2 points while they gain 3 points. If you choose BETRAY while they choose ALLY, you will receive 3 points and they will lose 2.\n4. You need at least 9 points to win the game.\n5. If your score will drop below 0, you will be punished.\nHave a nice tragedy!\n")
+
+Player_A = str(input("Please provide your name: "))
+
+Player_B = random.choice(enemy_names)
+enemy_names.remove(Player_B)
+
+Player_C = random.choice(enemy_names)
+
+# setting the score
+
+player_score = 0
+opponent_score = 0
+
+# pairing up
+
+pairing = random.choice(true_false)
+
+if pairing == "true":
+    print(f"You are playing as a PAIR. Your partner is {Player_B}.")
+    print(f"Your opponent is {Player_C}.\n")
+
+    for score in range(0, 100):
+        if player_score >= 9 or opponent_score >= 9:
+            print("The game has concluded.")
+            if player_score >= 9:
+                print("You have won the Ambidex Game. Congratulations! Please go to Door 9.")
+                input("Press any key to quit.")
+                sys.exit()
+                
+            elif opponent_score >= 9:
+                print("You have lost the Ambidex Game. Please await your punishment.")
+                input("Press any key to quit.")
+                sys.exit()
+
+        elif player_score < 0 or opponent_score < 0:
+            if opponent_score < 0:
+                print("You have won the Ambidex Game. Congratulations! Please go to Door 9.")
+                input("Press any key to quit.")
+                sys.exit()
+                
+            elif player_score < 0:
+                print("You have lost the Ambidex Game. Please await your punishment.")
+                input("Press any key to quit.")
+                sys.exit()
+
+# the main game logic 
+                
+        else:
+            print(f"It is your turn, {Player_A}. Do you wish to Ally or Betray {Player_C}?")
+            player_choice = input("Please provide your answer by typing either ALLY or BETRAY: ")
+
+            if player_choice != "ALLY" and player_choice != "BETRAY":
+                print("\n!!! Unknown command. Round will be skipped. !!!\n")
+            
+            else:
+                opponent_choice = random.choice(ambidex)
+                print(f"{Player_A} and {Player_B} have chosen {player_choice}.")
+                print(f"{Player_C} has chosen {opponent_choice}.")
+
+
+            if player_choice == "ALLY" and opponent_choice == "ALLY":
+                player_score += 2
+                opponent_score += 2
+            elif player_choice == "BETRAY" and opponent_choice == "BETRAY":
+                player_score += 0
+                opponent_score += 0
+            elif player_choice == "ALLY" and opponent_choice == "BETRAY":
+                player_score -= 2
+                opponent_score += 3
+            elif player_choice == "BETRAY" and opponent_choice == "ALLY":
+                player_score += 3
+                opponent_score -= 2
+            
+            print(f"{Player_A} and {Player_B}'s score is {player_score}.")
+            print(f"{Player_C}'s score is {opponent_score}.\n")
+
+if pairing == "false":
+    print(f"You are playing as a SOLO.")
+    print(f"Your opponents are {Player_B} and {Player_C}.\n")
+
+    while player_score >= 9 or opponent_score >= 9 or player_score < 0 or opponent_score < 0:
+        if player_score >= 9 or opponent_score >= 9:
+            print("The game has concluded.")
+            if player_score >= 9:
+                print("You have won the Ambidex Game. Congratulations! Please go to Door 9.")
+                input("Press any key to quit.")
+                sys.exit()
+                
+            elif opponent_score >= 9:
+                print("You have lost the Ambidex Game. Please await your punishment.")
+                input("Press any key to quit.")
+                sys.exit()
+
+        elif player_score < 0 or opponent_score < 0:
+            if opponent_score < 0:
+                print("You have won the Ambidex Game. Congratulations! Please go to Door 9.")
+                input("Press any key to quit.")
+                sys.exit()
+                
+            elif player_score < 0:
+                print("You have lost the Ambidex Game. Please await your punishment.")
+                input("Press any key to quit.")
+                sys.exit()
+
+# the main game logic 
+                
+        else:
+            print(f"It is your turn, {Player_A}. Do you wish to Ally or Betray {Player_B} and {Player_C}?")
+            player_choice = input("Please provide your answer by typing either ALLY or BETRAY: ")
+
+            if player_choice != "ALLY" and player_choice != "BETRAY":
+                print("\n!!! Unknown command. Round will be skipped. !!!\n")
+            
+            else:
+                opponent_choice = random.choice(ambidex)
+                print(f"{Player_A} has chosen {player_choice}.")
+                print(f"{Player_B} and {Player_C} have chosen {opponent_choice}.")
+
+            if player_choice == "ALLY" and opponent_choice == "ALLY":
+                player_score += 2
+                opponent_score += 2
+            elif player_choice == "BETRAY" and opponent_choice == "BETRAY":
+                player_score += 0
+                opponent_score += 0
+            elif player_choice == "ALLY" and opponent_choice == "BETRAY":
+                player_score -= 2
+                opponent_score += 3
+            elif player_choice == "BETRAY" and opponent_choice == "ALLY":
+                player_score += 3
+                opponent_score -= 2
+            
+            print(f"{Player_A}'s score is {player_score}.")
+            print(f"{Player_B} and {Player_C}'s score is {opponent_score}.\n")
---
+++ python/pokedex/functions.py
@@ -0,0 +1,226 @@
+import sys
+import lists
+
+# default values
+user_action = 0
+name = ""
+pokemon_name = ""
+pokemon_id = 0
+
+def body():
+    intro()
+    main_menu()
+
+def intro():
+    print("Welcome to PyPokedex!\nPlease note that is only supports Gen 1 Pokemon list.\n")
+    global name
+    name = input("Please provide your name: ")
+    print(f"Hello, {name}!")
+
+def main_menu():
+    global user_action
+    print("\nAvailable actions:\n1. List of all Generation 1 Pokemon.\n2. Search Pokemon by PyPokedex ID.\n3. Check Pokemon's PyPokedex ID.\n4. List of all Pokemon caught.\n5. Add Pokemon to the caught list.\n6. List of all Pokemon yet to be caught.\n7. Check Pokemon details (name, PyPokedex ID, if caught).\n8. Check your stats.\n\nIf you want to exit PyPokedex, please type in 2137.\n\n")
+    user_action = int(input("Please type the number of an option you want to go to. "))
+    action()
+
+def action():
+    global user_action
+    global pokemon_id
+    global pokemon_name
+
+    if user_action == 1:
+        all_pokemon_list()
+
+    elif user_action == 2:
+        search_by_id()
+
+    elif user_action == 3:
+        search_by_name()
+
+    elif user_action == 4:
+        caught_pokemon_list()
+
+    elif user_action == 5:
+        add_pokemon()
+
+    elif user_action == 6:
+        remaining_pokemon_list()
+
+    elif user_action == 7:
+        pokemon_details()
+
+    elif user_action == 8:
+        stats()
+
+    elif user_action == 2137:
+        input("\n\nPress any key to confirm.")
+        sys.exit()
+
+    else:
+        print("\n\n!!! Incorrect number. Please use a number of a corresponding option above. !!!\n\n")
+        user_action = int(input("Please type the number of an option you want to go to. "))
+        action()
+
+
+def search_by_id():
+    global pokemon_id
+    global pokemon_name
+
+    print("This section allows you to search Pokemon's name by their PyPokedex ID. Please note that after displaying the name PyPokedex will go back to main menu.\n")
+    pokemon_id = int(input("\nType in PyPokedex ID of Pokemon you are looking for: "))
+
+    for key, value in lists.pokemon_list.items():
+        if value == pokemon_id:
+            pokemon_name = key
+            print(f"\nPokemon that has Pokedex ID {pokemon_id} is: {pokemon_name}.\n")
+    
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def search_by_name():
+    global pokemon_id
+    global pokemon_name
+
+    print("This section allows you to search Pokemon's ID by their name. Please note that after displaying the value PyPokedex will go back to main menu.\n")
+    pokemon_name = input("\nType in name of Pokemon you are looking for: ")
+
+    pokemon_id = lists.pokemon_list[pokemon_name]
+    print(f"\nPokedex ID of {pokemon_name} is: {pokemon_id}.")
+    
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def all_pokemon_list():
+    global pokemon_id
+
+    print("\nFull list of all Generation 1 Pokemon:\n\n")
+
+    for pokemon in lists.pokemon_list:
+        pokemon_id = lists.pokemon_list[pokemon]
+        print(f"{pokemon_id}. {pokemon}")
+        pokemon_id += 1
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def caught_pokemon_list():
+    global pokemon_id
+
+    if not lists.caught_pokemon_list:
+        print("\nYou have not caught any Pokemon yet.")
+
+    else:
+        print("\nFull list of all Generation 1 Pokemon that you have caught:\n\n")
+
+        for pokemon in lists.caught_pokemon_list:
+            pokemon_id = lists.caught_pokemon_list[pokemon]
+            print(f"{pokemon_id}. {pokemon}")
+            pokemon_id += 1
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def remaining_pokemon_list():
+    global pokemon_id
+
+    if not lists.caught_pokemon_list:
+        print("\nYou have caught all Pokemon, congratulations!")
+
+    else:
+        print("\nFull list of all Generation 1 Pokemon that you have not yet caught:\n\n")
+
+        for pokemon in lists.remaining_pokemon_list:
+            pokemon_id = lists.remaining_pokemon_list[pokemon]
+            print(f"{pokemon_id}. {pokemon}")
+            pokemon_id += 1
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def pokemon_details():
+    global pokemon_id
+    global pokemon_name
+    global name
+
+    choice = input("\nDo you want to search by NAME or ID? (Please use all caps) ")
+
+
+    if choice == "NAME":
+        pokemon_name = input("\nType in name of Pokemon you are looking for: ")
+
+        if pokemon_name in lists.pokemon_list.keys():
+            pokemon_id = lists.pokemon_list[pokemon_name]
+
+            caught_check = pokemon_name
+
+            if caught_check in lists.caught_pokemon_list.keys():
+                print(f"\nPokemon name: {pokemon_name}\nPokemon ID: {pokemon_id}\nPokemon has been caught by {name}.")
+            
+            else:
+                print(f"\nPokemon name: {pokemon_name}\nPokemon ID: {pokemon_id}\nPokemon hasn't been caught.")
+
+        else:
+                print("\n\n!!! Incorrect value. Please use a correct value (either Pokemon ID or Pokemon's name). !!!\n\n")
+                pokemon_details()
+    
+    elif choice == "ID":
+        pokemon_id = int(input("\nType in ID of Pokemon you are looking for: "))
+
+        if pokemon_id in lists.pokemon_list.values():
+            
+            for key, value in lists.pokemon_list.items():
+                if value == pokemon_id:
+                    pokemon_name = key
+
+            caught_check = pokemon_name
+
+            if caught_check in lists.caught_pokemon_list.keys():
+                print(f"\nPokemon name: {pokemon_name}\nPokemon ID: {pokemon_id}\nPokemon has been caught by {name}.")
+            
+            else:
+                print(f"\nPokemon name: {pokemon_name}\nPokemon ID: {pokemon_id}\nPokemon hasn't been caught.")
+
+        else:
+                print("\n\n!!! Incorrect value. Please use a correct value (either Pokemon ID or Pokemon's name). !!!\n\n")
+                pokemon_details()
+                
+    elif choice != "ID" and choice != "NAME":
+        print("\n\n!!! Incorrect value. Please use a correct value (either Pokemon ID or Pokemon's name). !!!\n\n")
+        pokemon_details()
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def add_pokemon():
+    global pokemon_id
+    global pokemon_name
+
+    pokemon_name = input("\nType in name of Pokemon you have caught: ")
+    pokemon_id = lists.pokemon_list[pokemon_name]
+
+    lists.caught_pokemon_list[pokemon_name] = pokemon_id
+
+    del lists.remaining_pokemon_list[pokemon_name]
+
+    print(f"\nCongrats on catching {pokemon_name}! Your new friend has been added to the list.\n")
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
+def stats():
+    global name
+
+    amount_caught = len(lists.caught_pokemon_list)
+    amount_remaining = len(lists.remaining_pokemon_list)
+
+    if not lists.caught_pokemon_list:
+        print(f"\n{name}'s Pokemon stats:\nPokemon caught: {amount_caught}\nPokemon not yet caught: {amount_remaining}\n")
+
+    else:
+        last_pokemon = list(lists.caught_pokemon_list)[-1]
+
+        print(f"\n{name}'s Pokemon stats:\nPokemon caught: {amount_caught}\nPokemon not yet caught: {amount_remaining}\nLast caught Pokemon: {last_pokemon}.\n")
+
+    input("\nPress any button go to back to main menu.")
+    main_menu()
+
